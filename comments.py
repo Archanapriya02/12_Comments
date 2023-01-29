6427db9541fbb29c6a1df0ceed3d9542fb50e8d9
Archanapriya02 <123094628+Archanapriya02@users.noreply.github.com>
2023-01-29 12:46:44 +0530

    Add files via upload

---
+++ comments.py
@@ -0,0 +1,17 @@
+#print('python program')
+#print("python program")
+
+
+#print('python pro'gram') this line will not work
+#print("python pro"gram")
+
+      
+print('python pro\'gram')
+print("python pro\"gram")
+      
+      
+
+
+
+
+
